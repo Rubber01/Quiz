4b7ca7e3aae35a694db6a2924450571e77b3348f
Rubber01 <36899252+Rubber01@users.noreply.github.com>
2022-02-04 08:21:49 +0100

    Vecchia versione di Javadoc

--- doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"app"},{"p":"<Unnamed>","l":"Eccezioni"},{"p":"<Unnamed>","l":"Estrai"},{"p":"<Unnamed>","l":"Main"},{"p":"<Unnamed>","l":"registration"},{"p":"<Unnamed>","l":"Valid"}];updateSearchResults();
